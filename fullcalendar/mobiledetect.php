4e017c479e4d89e8d0052209e864a94bff288ac2
ahilaa <akilakamuthurai@gmail.com>
2016-10-23 11:50:32 -0700

    iteration1

---
+++ fullcalendar/mobiledetect.php
@@ -0,0 +1,52 @@
+<script>
+    var naviga="Mozilla/5.0 (Linux; Android 6.0.1; SAMSUNG SM-G900T Build/MMB29M) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/4.0 Chrome/44.0.2403.133 Mobile Safari/537.36";
+
+    alert(naviga);
+    $is_mobile=false;
+        if( /Android|webOS|iPhone|iPod|iPad|Windows Phone|BlackBerry|Linux; Android|IEMobile|Opera Mini/i.test(naviga) ) {
+        $is_mobile = true;
+        }
+ var host_pathname = window.location.pathname;
+                var exclude_path_1 = "/hollywood/2016/apsocial/";
+                var exclude_path_2 = "/hollywood/2016/apsite/";
+                var exclude_path_3 = "/hollywood/2016/eyegore_awards.php";
+                var exclude_path_4 = "/burgerking/";
+                var exclude_path_5 = "/hollywood/2016/burgerking.php";
+                var exclude_path_6 = "/hollywood/2016/pm_social/";
+                var exclude_path_7 = "/hollywood/2016/pm_web/";
+                var exclude_path_8 = "/hollywood/2016/pm_email/";
+                var exclude_path_9 = "/pass/";
+                var exclude_path_10 = "/passmember/";
+                var exclude_path_11 = "/hollywood/2016/the-walking-dead.php";
+                var exclude_path_12 = "/hollywood/2016/overview.php/";
+                var exclude_path_13 = "/hollywood/2016/tickets.php/";
+                var exclude_path_14 = "/hollywood/2016/know_before_you_go.php/";
+                var exclude_path_15 = "/hollywood/2016/news_review.php/";
+                var exclude_path_16 = "/hollywood/2016/faqs.php/";
+                var exclude_path_17 = "/hollywood/2016/directions.php/";
+                var exclude_path_18 = "/hollywood/2016/att-american-horror-story.php/";
+                var exclude_path_19 = "/hollywood/2016/att-the-exorcist.php/";
+                var exclude_path_20 = "/hollywood/2016/att-halloween.php/";
+                var exclude_path_21 = "/hollywood/2016/att-texas-chainsaw.php/";
+                var exclude_path_22 = "/hollywood/2016/att-freddy-vs-jason.php/";
+                var exclude_path_23 = "/hollywood/2016/att-krampus.php/";
+                var exclude_path_24 = "/hollywood/2016/terror_tram.php/";
+                var exclude_path_25 = "/hollywood/2016/show.php/";
+                var exclude_path_26 = "/hollywood/2016/rides.php/";
+                var exclude_path_27 = "/hollywood/2016/scare_zones.php/";
+                var exclude_path_28 = "/hollywood/2016/gallery.php/";
+                var exclude_path_29 = "/hollywood/2016/videos.php/";
+                var exclude_path_30 = "/hollywood/2016/videos_commercial.php/";
+                var exclude_path_31 = "/hollywood/2016/videos_guest_reactions.php/";
+                var exclude_path_32 = "/hollywood/2016/videos_behind_the_scenes.php/";
+                var exclude_path_33 = "/hollywood/2016/Front_Of_Line_ticket.php/";
+                var exclude_path_34 = "/hollywood/2016/day_night_combo.php/";
+                var exclude_path_35 = "/hollywood/2016/passmemberv2.php";
+                var exclude_path_36 = "/bk/";
+
+                if($is_mobile && host_pathname != exclude_path_1 && host_pathname != exclude_path_2 && host_pathname != exclude_path_3 && host_pathname != exclude_path_4 && host_pathname != exclude_path_5 && host_pathname != exclude_path_6 && host_pathname != exclude_path_7 && host_pathname != exclude_path_8 && host_pathname != exclude_path_9 && host_pathname != exclude_path_10 && host_pathname != exclude_path_11 && host_pathname != exclude_path_12 && host_pathname != exclude_path_13 && host_pathname != exclude_path_14 && host_pathname != exclude_path_15 && host_pathname != exclude_path_16 && host_pathname != exclude_path_17 && host_pathname != exclude_path_18 && host_pathname != exclude_path_19 && host_pathname != exclude_path_20 && host_pathname != exclude_path_21 && host_pathname != exclude_path_22 && host_pathname != exclude_path_23 && host_pathname != exclude_path_24 && host_pathname != exclude_path_25 && host_pathname != exclude_path_26 && host_pathname != exclude_path_27 && host_pathname != exclude_path_28 && host_pathname != exclude_path_29 && host_pathname != exclude_path_30 && host_pathname != exclude_path_31 && host_pathname != exclude_path_32 && host_pathname != exclude_path_33 && host_pathname != exclude_path_34 && host_pathname != exclude_path_35 && host_pathname != exclude_path_36 ){
+                        window.location.href = "http://m.halloweenhorrornights.com/hollywood";
+                }
+
+    alert($is_mobile);
+</script>
---
+++ fullcalendar/process.php
@@ -0,0 +1,137 @@
+<?php
+session_start();
+include('config.php');
+
+$type = $_POST['type'];
+
+if(strlen($type)<=0){
+	$type= $_GET['type'];
+}
+
+
+$lecid=$_POST[lecid];
+
+if(strlen($lecid)<=0){
+	$lecid= $_SESSION[lecid];
+}
+//Override default zone
+$_POST['zone']= "07:00";
+
+if($type == 'new')
+{
+	$startdate = $_POST['startdate'].'-'.$_POST['zone'];
+	$title = $_POST['title'];
+	$insert = mysqli_query($con,"INSERT INTO calendar(`title`, `startdate`, `enddate`,`lecid`, `allDay`) VALUES('$title','$startdate','$startdate','$_SESSION[lecid]','false')");
+	$lastid = mysqli_insert_id($con);
+	//sendmail ($_SESSION["stu_email"], $_SESSION["lec_email"], "APPONTMENT REQUEST", "Blah blah" );
+
+	//$headers = "From: ".$fromemail . "\r\n" . 	"CC: ganesh.euraka@gmail.com";
+	// Always set content-type when sending HTML email
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+    // More headers
+    //$headers .= 'From: '.$_SESSION["stu_email"] . "\r\n";
+    //$headers .= 'Cc: myboss@example.com' . "\r\n";
+
+    $headers = 'From: '.$_SESSION["stu_email"] . "\r\n" .
+        'Reply-To:' .$_SESSION["lec_email"] . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+	//echo "sdsds".$headers;
+
+	$url="http://localhost/kashipara.com_student-managment-system-zip/student%20managment%20system/StudentInformationProject/fullcalendar/process.php?type=accept&eventid=".$lastid;
+	$message= "Hi ".$lecid."</br>";
+	$message .= $_SESSION['stu_email']." has requested for appointment between ".$startdate." to ".$startdate."</br>";
+	$message .= "To confirm please <a href=".$url.">click here</a>";
+
+	 echo mail($_SESSION["lec_email"], "APPONTMENT REQUEST", $message,$headers);
+
+	echo json_encode(array('status'=>'success','eventid'=>$lastid,'lecid'=>$_POST[type]));
+}
+
+if($type == 'accept')
+{
+	$eventid = $_GET['eventid'];
+
+	$update = mysqli_query($con,"UPDATE calendar SET status='accepted' where id='$eventid'");
+	if($update){
+		echo json_encode(array('status'=>'success'));
+		echo "Accepted";
+	}else{
+		echo json_encode(array('status'=>'failed'));
+		echo "Declined";
+	}
+}
+
+if($type == 'changetitle')
+{
+	$eventid = $_POST['eventid'];
+	$title = $_POST['title'];
+	$update = mysqli_query($con,"UPDATE calendar SET title='$title' where id='$eventid'");
+	if($update)
+		echo json_encode(array('status'=>'success'));
+	else
+		echo json_encode(array('status'=>'failed'));
+}
+
+if($type == 'resetdate')
+{
+	$title = $_POST['title'];
+	$startdate = $_POST['start'];
+	$enddate = $_POST['end'];
+	$eventid = $_POST['eventid'];
+	$update = mysqli_query($con,"UPDATE calendar SET title='$title', startdate = '$startdate', enddate = '$enddate' where id='$eventid'");
+	if($update)
+		echo json_encode(array('status'=>'success'));
+	else
+		echo json_encode(array('status'=>'failed'));
+}
+
+if($type == 'remove')
+{
+	$eventid = $_POST['eventid'];
+	$delete = mysqli_query($con,"DELETE FROM calendar where id='$eventid'");
+	if($delete)
+		echo json_encode(array('status'=>'success'));
+	else
+		echo json_encode(array('status'=>'failed'));
+}
+
+if($type == 'fetch')
+{   //echo $_SESSION['userid'];
+	$events = array();
+
+	if($_SESSION["type"]=="admin")
+	{
+		$query = mysqli_query($con, "SELECT * FROM calendar' ");
+	}
+	else if ($_SESSION["type"]=="student")
+	{
+		$query = mysqli_query($con, "SELECT * FROM calendar where lecid= '$_SESSION[lecid]' ");
+	}
+
+	while($fetch = mysqli_fetch_array($query,MYSQLI_ASSOC))
+	{
+	$e = array();
+    $e['id'] = $fetch['id'];
+    $e['title'] = $fetch['title'];
+    $e['start'] = $fetch['startdate'];
+    $e['end'] = $fetch['enddate'];
+    $e['status'] = $fetch['status'];
+    $e['lecid'] = $_SESSION['lecid'];
+
+    $allday = ($fetch['allDay'] == "true") ? true : false;
+    $e['allDay'] = $allday;
+
+    array_push($events, $e);
+	}
+	echo json_encode($events);
+}
+
+/*function sendmail ($fromemail, $toemail, $subject, $message ){
+
+	$headers = "From: ".$fromemail . "\r\n" . 	"CC: ganesh.euraka@gmail.com";
+	 mail($toemail,$subject,$message,$headers); }*/
+
+
+?>
