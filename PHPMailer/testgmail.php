39721af7a91ecab33bf299b24875f9fe845a8191
ahilaa <akilakamuthurai@gmail.com>
2016-12-21 11:46:35 -0800

    update

--- PHPMailer/testgmail.php
+++ PHPMailer/testgmail.php
@@ -51,7 +51,7 @@ $mail->SMTPAuth = true;
 $mail->Username = "csungroupprojectsem1@gmail.com";
 
 //Password to use for SMTP authentication
-$mail->Password = "family17";
+$mail->Password = "******";
 
 //Set who the message is to be sent from
 $mail->setFrom('csungroupprojectsem1@gmail.com', 'csungroupprojectsem1');
